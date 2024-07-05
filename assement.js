7bfe5d0a3d37fab75c57161ef393c419838002a3
Pranay969 <pranay.chalapati@education.nsw.gov.au>
2024-07-05 10:56:39 +1000

    Add files via upload

---
+++ assement.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('get-started-btn');
+
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
