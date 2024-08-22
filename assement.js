35e06ebab51154f9015987632b5bab896b7197ba
Pranay969 <pranay.chalapati@education.nsw.gov.au>
2024-08-22 21:13:53 +1000

    Delete assement.js

--- assement.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('get-started-btn');
-
-    button.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
-});
